bb04018a2dc8ba3a0a190539ddc9b7af91007f31
OsborneLinus <linusholm1997@gmail.com>
2024-03-05 18:12:16 +0100

    quickfix with changing the name of a reference to a model

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,26 +9,26 @@
             <img src="" class="object-cover w-full h-full text-center" alt="Profile img">
         </div>
         <div class="profile-user-info">
-            <p class="text-xl text-center m-4">{{$user['name']}}</p>
-            <p class="text-xl text-center m-0">{{$user['email']}}</p>
+            <p class="text-xl text-center m-4">{{ $user['name'] }}</p>
+            <p class="text-xl text-center m-0">{{ $user['email'] }}</p>
         </div>
     </div>
 </section>
 <div class="line bg-gray-300 h-px w-full m-4"></div>
 <section class="user-posts flex justify-center">
-    @if($posts->isEmpty())
+    @if ($posts->isEmpty())
         <p>No posts available.</p>
     @else
         @foreach ($posts as $post)
             <div class="border-2 my-4">
                 <h2 class="text-lg font-semibold">{{ $post->title }}</h2>
                 <a class="underline text-sm"
-                href="{{ route('categories_show', $post->category->id) }}">{{ $post->category->name }}</a>
+                    href="{{ route('categories_show', $post->CarBrand->id) }}">{{ $post->CarBrand->name }}</a>
                 <p>{{ $post->body }}</p>
                 <p class="italic text-sm">Written by {{ $post->user->name }}</p>
                 <p class="italic text-sm">{{ $post->date }}</p>
             </div>
-    
+
             <form method="post" action="{{ route('dashboard.delete-post', $post) }}">
                 @csrf
                 @method('DELETE')
