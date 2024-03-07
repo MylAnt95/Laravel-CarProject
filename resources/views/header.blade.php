24f5b04b48d1d31fbc18e2392146b6840e110641
MylAnt95 <anton.myllykoski95@gmail.com>
2024-03-07 10:27:30 +0100

    Added styles and fixed img scaling on news

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,10 +2,11 @@
 <header>
     <nav class="flex justify-between bg-white shadow-md mb-10">
         <div class="w-full flex justify-between">
-            <div class="nav-left px-4 py-2 text-black-800 font-bold">
-                <a href="/">
+            <div class="nav-left px-4 py-2 text-black-800 font-bold flex flex-row">
+                <a class="self-center mr-1" href="/">
                     <h4>Car Forum</h4>
                 </a>
+                <img src="{{ asset('images/car.svg') }}" alt="car logo">
             </div>
             @auth
                 <div class="nav-mid flex gap-3 px-4 py-2 text-gray-800">
@@ -17,7 +18,7 @@
                     <a class="hover:opacity-50 hover:scale-105" href="">Lorem</a>
                 </div>
             @endauth
-            <div class="nav-right flex flex-row gap-3 px-4 py-2 text-gray-800">
+            <div class="nav-right flex flex-row gap-3 px-4 py-2 text-gray-800 self-center">
                 {{-- User authentication links --}}
                 @guest
                     {{-- Show login and register links for guest users &  --}}
