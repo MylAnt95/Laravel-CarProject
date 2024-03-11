c5dcdd8b7e062dfc6081e7258041ed6349ccb570
MylAnt95 <anton.myllykoski95@gmail.com>
2024-03-11 14:21:46 +0100

    added footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,24 @@
+<footer class="flex justify-between bg-white p-5 flex-col md:flex-row z-999 gap-8">
+    <div class="footer-logo md:self-start self-center flex flex-row">
+        <a class="self-center mr-1 w-full" href="/">
+            <h4 class="">Car Forum</h4>
+        </a>
+        <img src="{{ asset('images/car.svg') }}" alt="car logo">
+    </div>
+    <div class="footer-items flex flex-row gap-5 justify-center">
+        <div class="col-1 flex flex-col">
+            <div class="flex flex-col mt-4vh md:mt-0">
+                <h3 class="">Contact</h3>
+                <span class="">071 99 32 94</span>
+                <span class="">info@carforum.se</span>
+            </div>
+        </div>
+        <div class="col-2 flex flex-col">
+            <div class="flex flex-col mt-4vh md:mt-0">
+                <h3 class="">Address</h3>
+                <span class="">Göte 23</span>
+                <span class="">445 32 Göteborg</span>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,10 @@
 @include('errors')
 @extends('layouts.app')
 @include('header')
-<div class="flex flex-col md:flex-row">
+<div class="flex flex-col md:flex-row mb-20">
     {{------------ Forum Talk ------------}}
-    <div class="flex flex-col max-h-[428px] md:max-h-none overflow-hidden md:justify-center">
-        <h2 class="text-2xl font-bold text-center md:text-start">Forum talk</h2>
+    <div class="flex flex-col max-h-[428px] md:max-h-none overflow-hidden">
+        <h2 class="text-2xl font-bold text-center md:text-start mt-24 md:mt-0">Forum talk</h2>
         <div class="md:w-96 md:h-fit md:sticky md:top-0 px-5 md:pr-10 md:max-h-screen md:overflow-y-scroll">
             @foreach ($posts as $post)
                 <div class="chat-container bg-white rounded-3xl border-2 my-10 p-3">
@@ -27,7 +27,7 @@
     <div class="Line md:hidden w-full h-0.5 bg-gray-300">
     </div>
     {{------------ News ------------}}
-    <div class="md:w-2/3 md:border-l-2 px-5 md:pl-10 border-gray-300 flex flex-column flex-wrap gap-20 md:gap-50 max-h-screen">
+    <div class="md:w-2/3 md:border-l-2 px-5 md:pl-10 border-gray-300 flex flex-column flex-wrap gap-20 md:gap-50">
         <h2 class="text-2xl font-bold hidden md:block">News</h2>
         <div class="news-top flex flex-col md:flex-row border-b-2">
             <h2 class="text-2xl font-bold text-center my-10 md:hidden">News</h2>
@@ -122,3 +122,4 @@
 </div>
 
 </div>
+@include('footer')
