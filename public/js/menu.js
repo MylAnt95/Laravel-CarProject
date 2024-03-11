c3e2e50ce69e2d85c5a036396f725b9da79db1bd
MylAnt95 <anton.myllykoski95@gmail.com>
2024-03-11 13:27:53 +0100

    Fixed responsitivity on header and added dropdown menu with a hamburger svg

---
+++ public/js/menu.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const dropMenu = document.querySelector(".drop-menu");
+let isOpen = false;
+
+// Get taiilwind classes from app.css
+dropMenu.classList.add('animate-dropDown');
+dropMenu.classList.add('origin-top-center');
+
+hamburger.addEventListener("click", () => {
+
+    dropMenu.classList.toggle('hidden');
+    dropMenu.classList.toggle('flex');
+    isOpen = !isMenuOpen;
+});
