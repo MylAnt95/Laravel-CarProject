757b40ff23bbdb3e81bc6045126b7f969c82a8ae
MylAnt95 <anton.myllykoski95@gmail.com>
2024-03-12 13:47:52 +0100

    Added route to dashboard for authenticated users

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -11,9 +11,9 @@
             @auth
                 <div class="nav-mid hidden md:flex gap-3 px-4 py-2 self-center text-gray-800">
                     {{-- Navigation links for logged in users --}}
-                    <a href="/dashboard" class="hover:opacity-50 hover:scale-105" href="">Forum</a>
+                    <a href="/dashboard" class="hover:opacity-50 hover:scale-105">Forum</a>
                     <div class="line-separator bg-gray-200 h-auto w-0.5"></div>
-                    <a href="dashboard/create-post" class="hover:opacity-50 hover:scale-105"href="">Create a post</a>
+                    <a href="dashboard/create-post" class="hover:opacity-50 hover:scale-105">Create a post</a>
                     <div class="line-separator bg-gray-200 h-auto w-0.5"></div>
                     <a class="hover:opacity-50 hover:scale-105" href="">Lorem</a>
                 </div>
@@ -27,9 +27,9 @@
                 <div class="drop-menu hidden w-full h-fit top-12 flex-col fixed inset-0 items-center justify-center bg-white shadow-lg z-10">
 
                     {{-- Dropmenu links --}}
-                    <a href="/dashboard" class="block px-4 py-2 active:opacity-50" href="">Forum</a>
+                    <a href="/dashboard" class="block px-4 py-2 active:opacity-50">Forum</a>
                     @auth
-                        <a href="dashboard/create-post" class="block px-4 py-2 active:opacity-50"href="">Create a post</a>
+                        <a href="dashboard/create-post" class="block px-4 py-2 active:opacity-50">Create a post</a>
                         <a class="block px-4 py-2 active:opacity-50" href="">Lorem</a>
                     @endauth
                     <div class="Line md:hidden w-full h-0.5 bg-gray-300 mt-3 mb-3">
