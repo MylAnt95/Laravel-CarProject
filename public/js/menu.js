c34e9fd968afd366d85ed2c9b11c2cd25e232d10
MylAnt95 <anton.myllykoski95@gmail.com>
2024-03-12 13:39:12 +0100

    typos

--- public/js/menu.js
+++ public/js/menu.js
@@ -3,12 +3,12 @@ const dropMenu = document.querySelector(".drop-menu");
 let isOpen = false;
 
 // Get taiilwind classes from app.css
-dropMenu.classList.add('animate-dropDown');
-dropMenu.classList.add('origin-top-center');
+dropMenu.classList.add("animate-dropDown");
+dropMenu.classList.add("origin-top-center");
 
 hamburger.addEventListener("click", () => {
 
-    dropMenu.classList.toggle('hidden');
-    dropMenu.classList.toggle('flex');
-    isOpen = !isMenuOpen;
+    dropMenu.classList.toggle("hidden");
+    dropMenu.classList.toggle("flex");
+    isOpen = !isOpen;
 });
