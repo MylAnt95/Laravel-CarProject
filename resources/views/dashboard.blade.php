81d9848b0e278a8c89e3ea504ce5449e146973a9
MylAnt95 <anton.myllykoski95@gmail.com>
2024-03-12 15:56:20 +0100

    Added style to forms, profile, fixed routes and moved edit to profile

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,26 +1,33 @@
 @include('header')
 
-<section class="pt-24">
-    <h1 class="text-2xl text-center">CAR FORUM</h1>
-    
-    <h2 class="text-xl font-semibold">POSTS</h2>
-    @if ($posts->isEmpty())
-        <p>No posts available.</p>
-    @else
+<section class="py-24 md:pt-1 flex flex-col mx-3 md:mx-10">
+    <div class="titles py-5 text-center flex flex-col">
+        <h1 class="text-2xl text-center">CAR FORUM</h1>
+        <div class="Line w-1/3 h-0.5 bg-gray-300 self-center my-1">
+        </div>
+        <h2 class="text-xl font-semibold self-center py-1">POSTS</h2>
+    </div>
+    <div class="posts mt-3 container grid grid-cols-1 gap-5 md:grid-cols-2">
+        @if ($posts->isEmpty())
+            <p>No posts available.</p>
+        @else
         @foreach ($posts as $post)
-            <div class="chat-container bg-white rounded-3xl border-2 my-10 p-3">
-                <h2 class="text-lg font-semibold">{{ $post->title }}</h2>
-                <a class="underline text-sm"
-                    href="{{ route('categories_show', $post->carBrand->id) }}">{{ $post->carBrand->name }}</a>
-                <p>{{ $post->body }}</p>
-                <p class="italic text-sm">Written by {{ $post->user->name }}</p>
-                <p class="italic text-sm">{{ $post->date }}</p>
+            <div class="chat-container bg-white rounded-3xl border-2 p-3">
+                <div class="flex flex-row justify-between">
+                    <h2 class="text-lg font-semibold">{{ $post->title }}</h2>
+                    <a class="underline text-sm self-center ml-1"
+                href="{{ route('categories_show', $post->carBrand->id) }}">#{{ $post->carBrand->name }}</a>
+                </div>
+                <p class="py-3 text-sm border-t-2">{{ $post->body }}</p>
+                <div class="chat-bottom flex justify-between mt-auto">
+                    <p class="italic text-xs text-gray-500">Written by {{ $post->user->name }}</p>
+                    <p class="italic text-xs text-gray-500">{{ $post->date }}</p>
+                </div>
             </div>
-            @if (Auth::id() === $post->user_id)
-                <a href="{{ route('posts.edit', $post) }}" class="btn btn-primary">Edit</a>
-            @endif
-        @endforeach
-    @endif
+                
+            @endforeach
+        @endif
+    </div>
 </section>
 
 @include('footer')
